e456559949c51cf598fbbf9888f3f9886b4836b1
malimedo2009 <mateo.medak5@gmail.com>
2020-07-23 21:09:33 +0200

    First commit

---
+++ ajme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ajme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
